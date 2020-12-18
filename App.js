858a658a168aff6fd86bb7550b7c591308e4939a
Alon Rubin <alonrubin2@gmail.com>
2020-12-18 10:57:06 +0200

    all done?

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import './App.css';
 import Avatar from './Avatar/Avatar';
 
 function App() {
+
   return (
     <div className="App">
-      <Avatar width={100} route="./assets/avatarIMG" isSquere={false}/>
+      <Avatar width={100} route="./assets/avatarIMG"/>
+      
     </div>
   );
 }
